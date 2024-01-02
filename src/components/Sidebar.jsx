eb6f838b254e5b2a681d67d0d77e538065235e9b
Ruan Cardoso dos Santos <cardosoruan2001@gmail.com>
2024-01-02 13:45:13 -0300

    feat: Sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,27 @@
+import styles from './Sidebar.module.css'
+import { PencilLine } from "@phosphor-icons/react";
+
+export function Sidebar() {
+  return(
+    <aside className={styles.sidebar}>
+      <img 
+      className={styles.cover}
+      src="https://images.unsplash.com/photo-1605379399642-870262d3d051?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/ruancxrdoso.png" />
+
+        <strong>Ruan Cardoso</strong>
+        <span>Front End Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
